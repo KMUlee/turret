861876cb34e8ab3ed85cb20c8182943558aedc24
KMUlee <iwon@kookmin.ac.kr>
2018-12-02 12:22:06 +0900

    code

--- GameLogic.py
+++ GameLogic.py
@@ -4,7 +4,7 @@ class Turret():
         self.turrets = 0
         self.turret = 0.3
         self.tile = []
-        self.array = []
+        self.array = [5,5]
 
     def getTurrets(self):
         return self.turrets
@@ -34,33 +34,26 @@ class Turret():
                 else:
                     list.append("-")
             self.tile.append(list)
+        n = self.array[0]
+        m = self.array[1]
+        self.tile = self.find(self.tile,n,m)
 
-        m = self.array[0]
-        n = self.array[1]
-        tile = self.tile
-        for i in range(m):
-            for j in range(n):
+    def find(self, tile,n,m):
+        X = [-1,0,1,-1,1,-1,0,1]
+        Y = [-1,-1,-1,0,0,1,1,1]
+        for y in range(n):
+            for x in range(m):
                 num = 0
-                if tile[i][j] == "-":
-                    if j != 0 and tile[i][j - 1] == "*":
-                        num += 1
-                    if j != n - 1 and tile[i][j + 1] == "*":
-                        num += 1
-                    if i != 0 and tile[i - 1][j] == "*":
-                        num += 1
-                    if i != 0 and j != 0 and tile[i - 1][j - 1] == "*":
-                        num += 1
-                    if i != 0 and j != n - 1 and tile[i - 1][j + 1] == "*":
-                        num += 1
-                    if i != m - 1 and j != 0 and tile[i + 1][j - 1] == "*":
-                        num += 1
-                    if i != m - 1 and tile[i + 1][j] == "*":
-                        num += 1
-                    if i != m - 1 and j != n - 1 and tile[i + 1][j + 1] == "*":
-                        num += 1
-                    tile[i][j] = str(num)
+                if tile[y][x] == "-":
+                    for i in range(8):
+                        if 0 <= x + X[i] <= m - 1 and 0 <= y + Y[i] <= n - 1:
+                            if tile[y + Y[i]][x + X[i]] == "*":
+                                num += 1
+                    tile[y][x] = str(num)
+        return tile
 
 if __name__ == '__main__':
     t = Turret()
+    t.game()
     for i in t.tile:
         print(i)
